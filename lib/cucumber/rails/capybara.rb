3e909f464aef0a27199d4025b881b7989e4db009
Jonas Nicklas <jonas.nicklas@gmail.com>
2011-04-09 09:56:28 +0100

    Remove JavaScript emulation
    This no longer serves a purpose in Rails3, and
    it continually breaks when capybara changes its
    internals.

--- lib/cucumber/rails/capybara.rb
+++ lib/cucumber/rails/capybara.rb
@@ -2,6 +2,5 @@ if defined?(Capybara)
   require 'capybara/rails'
   require 'capybara/cucumber'
   require 'capybara/session'
-  require 'cucumber/rails/capybara/javascript_emulation'
   require 'cucumber/rails/capybara/select_dates_and_times'
 end
